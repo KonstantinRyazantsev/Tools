62fca06ab8adf14b0b4b5c740cc43f963c0aceb4
bijakatlykkex <bijakatlykkex@gmail.com>
2017-06-13 18:58:35 +0430

    Maximize disabled

--- OffchainHelper/MainForm.Designer.cs
+++ OffchainHelper/MainForm.Designer.cs
@@ -146,6 +146,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(743, 300);
             this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "OffchainHelper";
             this.tabControl1.ResumeLayout(false);
